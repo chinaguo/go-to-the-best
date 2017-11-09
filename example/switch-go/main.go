34af8e2195015c15f05a3ee45d0e498a005fc13f
chinaguo <chinaguoxinchun@126.com>
2017-11-09 17:56:32 +0800

    增加例子

--- example/switch-go/main.go
+++ example/switch-go/main.go
@@ -1,6 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
+
+type node struct {
+	name string
+}
+
+func (p *node) print() {
+	fmt.Println(p.name)
+}
 
 func main() {
 loop:
@@ -8,9 +19,35 @@ loop:
 		switch {
 		case true:
 			fmt.Println("break ...")
+			//跳出循环
 			break loop
 		}
 	}
 
+	//打印是随机的
+	data := []*node{{"one"}, {"two"}, {"three"}}
+	for _, v := range data {
+		go v.print()
+	}
+
+	time.Sleep(3 * time.Second)
+	fmt.Println("data!")
+	//one two three , v属于这个协程的
+	data2 := []node{{"one"}, {"two"}, {"three"}}
+	for _, v := range data2 {
+		v := v
+		go v.print()
+	}
+	time.Sleep(3 * time.Second)
+
+	dd := 1
+	dd++
+	//这个后面执行
+	defer fmt.Println("result =>", func() int { return dd * 2 }()) //print 2，这个在编译的时候已经确定了，使用的是最后的值
+	//后面的先执行
+	defer func() {
+		fmt.Println("dd: ", dd) //print 2,dd是有运行的时候进行编译的
+	}()
+	dd++
 	fmt.Println("exit!")
 }
